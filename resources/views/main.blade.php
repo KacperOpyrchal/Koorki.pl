e67153643a6b956f8098fcf356f8cc58f15c1666
Kacper Opyrchal <opyrchalkacper@gmail.com>
2017-07-06 23:56:27 +0200

    Quick fix commit

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -33,7 +33,7 @@
             <ul class="nav navbar-nav">
                 <li class="active"><a href="/">Strona główna <span class="sr-only">(current)</span></a></li>
                 <li><a href="#">Kategorie</a></li>
-                <li><a href="#">Rankingi</a></li>
+                <li><a href="/ranking">Rankingi</a></li>
                 <li><a href="#">Jak działa?</a></li>
                 <li><a href="/contact">Kontakt</a></li>
                 <li><a href="/about">O nas</a></li>
