ae66253b2c5bdd1fec8376036207f89d026e9be4
KarolSzawlis <k.szawlis@gmail.com>
2017-07-09 20:25:06 +0200

    Ranking page has been added

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,7 +4,11 @@
     <div class="row">
         <div class="col-md-12">
             <h1>O nas</h1>
-            <p>Jestesmy super zespolem</p>]
+            <p>Nasza strona przeznaczona jest dla osób, które oczkują pomocy i dla tych, którzy są w stanie jej udzielić.</p>
+            <p>Portal koorki.pl różni się od pozostałych stron internetowych tym, że zapewniamy połączenie video między dwoma osobami,</p>
+            <p>płatności realizujemy na trzy różne sposoby. Strona nie dotyczy wyłącznie korepetycji liczymy na to, że</p>
+            <p>będziecie udostepniać również wiekszę zadania, projekty czy sprawozdania. Będziemy starali się zapewnić jak </p>
+            <p>jak najlepszą jakoś świadczonych usług, nei pbieramy żadnych prowizji a ceny ustalacie wy. </p>
         </div>
     </div>
 @endsection
