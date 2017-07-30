4299d01d02009251732b136e17c554c64185d884
Kacper Opyrchal <opyrchalkacper@gmail.com>
2017-07-30 22:20:43 +0200

    1) Code rafactor

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    Udało sie zalogować :D
                 </div>
             </div>
         </div>
